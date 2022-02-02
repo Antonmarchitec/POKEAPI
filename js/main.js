bc378e4226739f8df776f17d472a8deda927cda2
mi nombre <desarrollowebaps@gmail.com>
2022-02-02 19:44:25 -0400

    GUARDANDO POKE2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-let template_poke = document.querySelector('#template_poke').content
 let cards = document.querySelector('#cards')
-let fragmentPoke = document.createDocumentFragment()
+let template_poke = document.querySelector('#template_poke').content
+let fragmentPoke = document.createDocumentFragment() 
 
 
 let boton = document.querySelector("#boton")
-//let fragmentPoke2 = document.createDocumentFragment()
+
+   
+ 
 
 
 
  
 
-boton.addEventListener("click", ()=>{
-    let random = getRamdonInt(1,501)
-    dataFech(random)
+document.addEventListener("DOMContentLoaded", () =>{  
+    //let random = getRamdonInt(1,501)
+    dataFech()
 })
-
 
 //FUNCION QUE GENERA UN NUMERO ALEATORIO
 const getRamdonInt = ( min , max ) =>{
@@ -22,15 +23,12 @@ const getRamdonInt = ( min , max ) =>{
 }
 
 
-const dataFech = async (random) =>{
+const dataFech = async () =>{
     try{
-      
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/")
         const data = await res.json()
         pintarPoke(data)
-        
-        
-
+      
     }catch(error){
         console.log("error")
     }
@@ -39,24 +37,50 @@ const dataFech = async (random) =>{
 
 
 
-
-
-
-
 const pintarPoke = (data) =>{
-   //console.log(data)
-   let clone = template_poke.cloneNode(true)
-   clone.querySelector('#name').textContent = data.name
-   clone.querySelector('img').setAttribute("src", data.sprites.other.dream_world.front_default)
-   clone.querySelector('#tipo').textContent = data.types[0].type.name
-   clone.querySelector('#mov').textContent = data.moves[3].move.name
-   clone.querySelector('#id').textContent = data.id
+    //console.log(data)
+    //console.log(data.next)
+    let pokemones_veinUno_cuarenta = data.next   //obtengo url pok 21-40
 
+    data.results.forEach(element => {
+        //console.log(element.name) 
+        //console.log(element.url) 
+            
+          
+           const dataFech2 = async () =>{
+               try{
+                   //LLAMADA AL JSON URL
+                   const res_2 = await fetch(element.url)
+                   const data_2 = await res_2.json()
+                   //console.log(data_2)
+                   pokemonesPintados(data_2)
 
-   fragmentPoke.appendChild(clone)
-   cards.appendChild(fragmentPoke)
+               }catch(error){
+                   console.log("error2")
+               }
+           }
+           dataFech2()
 
-    
+           const pokemonesPintados = (data_2) =>{
+                console.log(data_2)
+                
+                let clone = template_poke.cloneNode(true)
+                clone.querySelector("#name").textContent = data_2.name
+                clone.querySelector("img").setAttribute("src",data_2.sprites.other.dream_world.front_default)
+                clone.querySelector("#ID").textContent = data_2.id
+                clone.querySelector("#tipo").textContent = data_2.types[0].type.name
+                clone.querySelector("#mov1").textContent = data_2.moves[0].move.name
+                clone.querySelector("#mov2").textContent = data_2.moves[1].move.name
+              
+
+                fragmentPoke.appendChild(clone)
+                cards.appendChild(fragmentPoke)
+           }
+           //let random = getRamdonInt(1,601)
+         
+    }); 
+
 }
+
 
 
